7c840ede8c3f6ad77aef534378a3e60a8c9fc299
adam <bcuz@users.noreply.github.com>
2019-05-30 22:47:23 -0400

    add a dish

--- data/dishes-model.js
+++ data/dishes-model.js
@@ -4,11 +4,19 @@ module.exports = {
   get,
   // getById,
   // getCohortStudents,
-  // insert,
+  addDish,
   // update,
   // remove,
 };
 
 function get() {
   return db('dishes');
+}
+
+function addDish(dish) {
+  return db('dishes')
+    .insert(dish)
+    .then(ids => {
+      return ids[0]
+    });
 }
